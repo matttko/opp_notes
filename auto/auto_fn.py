9ee7b3a157b6b98cd1974596341d00694c86b3f5
ma <mat@MacBook-Air-Ala.local>
2023-01-14 13:55:46 +0100

    ostateczna wersja

---
+++ auto/auto_fn.py
@@ -0,0 +1,66 @@
+bmw = {
+    'model': 'e46',
+    'engine': False,
+    'speed': 0,
+    'max_speed': 160
+}
+
+fiat = {
+    'model': 'tipo',
+    'engine': False,
+    'speed': 0,
+    'max_speed': 240
+}
+# funkcja rozpozna fiat przez referencję do parametrów
+
+
+def auto_factory(model, max_speed): # ta funkcja zastąpi wypisywanie każdego modelu osobno
+    return {
+        'model': model,
+        'engine': False,
+        'speed': 0,
+        'max_speed': max_speed
+    }
+
+
+def start_engine(car):
+    if not car['engine']:
+        car['engine'] = True
+        print('Silnik odpalony')
+    else:
+        print('Silnik już był odpalony')
+
+
+def stop_engine(car):
+    if car['speed'] == 0:
+        car['engine'] = False
+        print('silnik zgaszony')
+
+    else:
+        print('zatrzymaj auto wpierw 😎')
+
+
+def speed_up(car, amount):
+    if car['engine']:
+        car['speed'] = min(car['speed'] + amount, car['max_speed'])
+        print(f'jedziesz z prędkością {car["speed"]}')
+    else:
+        print('odpal silnik wpierw')
+
+
+def slow_down(car, amount):
+    car['speed'] = max(car['speed'] - amount, 0)
+    print(f'jedziesz z prędkością {car["speed"]}')
+
+bmw = auto_factory('e46', 160)
+
+
+speed_up(bmw, 20)
+start_engine(bmw)
+speed_up(bmw, 20)
+speed_up(bmw, 50)
+speed_up(bmw, 200)
+stop_engine(bmw)
+slow_down(bmw, 50)
+slow_down(bmw, 250)
+stop_engine(bmw)
